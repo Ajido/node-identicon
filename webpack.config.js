02ef66e9f92b15ac2d5e108ae4a2b887931d4b80
Daiki Kuriyama <ajido@me.com>
2020-03-08 00:22:12 +0900

    fix vulnerability

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const path = require('path')
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'production',
   entry: {
     'identicon': path.join(__dirname, 'identicon.js'),
-    'identicon.min': path.join(__dirname, 'identicon.js'),
   },
-  devtool: '#source-map',
+  devtool: 'source-map',
   output: {
-    path: './dist',
+    path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     library: 'identicon',
     libraryTarget: 'umd'
@@ -19,12 +19,6 @@ module.exports = {
     canvas: 'canvas'
   },
   resolve: {
-    extensions: ['', '.js']
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+    extensions: ['.js']
+  }
 };
